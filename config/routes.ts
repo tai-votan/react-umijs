c1b81efd3139c60ab4432479d3046131bdcf3c74
Tài Võ <votantai@Tais-MacBook-Pro.local>
2022-04-17 01:41:40 +0700

    revert routes with layout

--- config/routes.ts
+++ config/routes.ts
@@ -1,22 +1,22 @@
 export default [
   {
-    // component: "@/layouts/main",
     path: "/",
     title: "site.title",
-    component: "@/pages/home",
-    // routes: [
-    //   {
-    //     path: "/",
-    //     title: "site.title",
-    //     component: "@/pages/home",
-    //   },
-    //   {
-    //     title: "404",
-    //     component: "@/pages/404",
-    //   },
-    // ],
+    component: "@/layouts/main",
+    routes: [
+      {
+        path: "/",
+        title: "site.title",
+        component: "@/pages/home",
+      },
+      {
+        title: "404",
+        component: "@/pages/404",
+      },
+    ],
   },
   {
+    path: "**",
     component: "@/pages/404",
   },
 ];
